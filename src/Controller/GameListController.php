d72e7c349903baa67e3ebf99493a7acdffb74a4e
AIELSOYY <alexandre.herisse21@gmail.com>
2024-03-06 15:01:56 +0100

    Added library and a lot of stuff

--- src/Controller/GameListController.php
+++ src/Controller/GameListController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\GamesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +16,7 @@ class GameListController extends AbstractController
     public function store(GamesRepository $repo): Response
     {
         $games = $repo->findall();
+        
         return $this->render('game_list/store.html.twig', [
             'controller_name' => 'GameListController',
             'games'=>$games
@@ -22,14 +24,51 @@ class GameListController extends AbstractController
     }
 
     /**
-     * @Route("/store/{id}", name="app_storePage")
+     * @Route("/store/{id}", name="app_storePaginated")
+     */
+    public function storePaginated($id, GamesRepository $repo): Response
+    {
+        $games = $repo->findall();
+        $nbGames = count($games);
+        $nbPages=(int) ($nbGames/8);
+
+        return $this->render('game_list/storePaginated.html.twig', [
+            'controller_name' => 'GameListController',
+            'games'=>$games,
+            'page'=>$id,
+            'nbGames'=>$nbGames,
+            'nbPages'=>$nbPages,
+        ]);
+    }
+
+    /**
+     * @Route("/store/game/{id}", name="app_storePage")
      */
     public function storePage($id, GamesRepository $repo): Response
     {
         $game = $repo->find($id);
-        return $this->render('game_list/gamePage.html.twig', [
+        return $this->render('game_list/storePage.html.twig', [
             'controller_name' => 'GameListController',
             'game'=>$game
         ]);
     }
+
+    /**
+     * @Route("/store/game/{id}/add", name="app_addGameToUser")
+     */
+    public function addGameToUser($id, GamesRepository $repo, EntityManagerInterface $entityManager): Response
+    {
+        $game = $repo->find($id);
+        $game->addUser($this->getUser());
+
+        $entityManager->persist($game);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_storePage',  ['id'=>$id]);
+
+        return $this->render('game_list/storePage.html.twig', [
+            'controller_name' => 'GameListController',
+        ]);
+    }
+
 }
